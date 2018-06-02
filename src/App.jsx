78fb67fbae082d14bfcae0832e7d0c19b57fc3e3
rodolfodosanjos <dosanjosrodolfo@gmail.com>
2018-06-02 23:46:26 +0000

    Fixed materialUI problems and created product Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ProductsMenu from './components/pages/ProductsMenu.jsx';
-import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
-import blue from 'material-ui/colors/blue';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import blue from '@material-ui/core/colors/blue';
 
 const theme = createMuiTheme({
   palette: {
--- src/components/pages/ProductsMenu.jsx
+++ src/components/pages/ProductsMenu.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import ProductCard from '../productsMenu/ProductCard.jsx';
 
 export default () => (
   <div>
-  	Oi mundo
+  	<ProductCard product={{
+  		imageSrc: 'https://cdn.pixabay.com/photo/2017/08/12/18/59/snack-2635035_640.jpg',
+  		name: 'Hamburguer com batata frita',
+  		category: 'hamburguer',
+  		description: 'Hamburguer com queijo, carne e batata frita',
+  		price: 40
+  	}}/>
   </div>
 );
