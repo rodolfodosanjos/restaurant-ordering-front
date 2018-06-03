0428657f2f51ffc45155871fb4ace36332cc4baf
rodolfodosanjos <dosanjosrodolfo@gmail.com>
2018-06-03 19:48:36 +0000

    Created order page

--- src/components/commons/CloseDialogButton.jsx
+++ src/components/commons/CloseDialogButton.jsx
@@ -5,18 +5,18 @@ import CloseIcon from '@material-ui/icons/Close';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({
-  closeDialogButton: {
-    position: 'absolute',
-    top: (theme.spacing.unit * 5),
-    right: (theme.spacing.unit * 5),
-    zIndex: 100
-  }
+	closeDialogButton: {
+		position: 'absolute',
+		top: (theme.spacing.unit * 5),
+		right: (theme.spacing.unit * 5),
+		zIndex: 100
+	}
 });
 
 const CloseDialogButton = ({classes, onClose}) => (
-  <Button mini onClick={onClose} className={classes.closeDialogButton}>
-    <CloseIcon/>
-  </Button>
+	<Button mini onClick={onClose} className={classes.closeDialogButton}>
+		<CloseIcon/>
+	</Button>
 );
 
 CloseDialogButton.propTypes = {
--- src/components/pages/Orders.jsx
+++ src/components/pages/Orders.jsx
@@ -1,8 +1,37 @@
 import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
 import OrderDialogOpenButton from '../orders/OrderDialogOpenButton.jsx';
+import OrdersComponent from '../orders/Orders.jsx';
+import PageTitle from '../commons/PageTitle.jsx';
+import Page from '../commons/Page.jsx';
 
-export default () => (
-	<div>
+const styles = theme => ({
+	ordersComponent: {
+		margin: '0 auto',
+		maxWidth: 500
+	},
+	[theme.breakpoints.up('lg')]: {
+		ordersComponent: {
+			maxWidth: 700
+		}
+	}
+});
+
+const Orders = ({classes}) => (
+	<Page>
+		<PageTitle>
+			Pedidos
+		</PageTitle>
+		<div className={classes.ordersComponent}>
+			<OrdersComponent />
+		</div>
 		<OrderDialogOpenButton />
-	</div>
+	</Page>
 );
+
+Orders.propTypes = {
+	classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles, {withTheme: true})(Orders);
